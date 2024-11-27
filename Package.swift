6417bdbcadc9d447cfeb1dc3db9b825e7979e98c
Kevin Galligan <kgalligan@gmail.com>
2024-11-27 16:01:29 -0500

    Update url

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://api.github.com/repos/kpgalligan/KMBTestDirectUrl/releases/assets/209494438.zip"
+let remoteKotlinUrl = "https://github.com/kpgalligan/KMBTestDirectUrl/releases/download/0.1.0/allshared.xcframework.zip"
 let remoteKotlinChecksum = "d0303162966336eac0b6b9c085bb9a0ed570f283da7f1b6df37e5946fbf52177"
 let packageName = "allshared"
 // END KMMBRIDGE BLOCK
